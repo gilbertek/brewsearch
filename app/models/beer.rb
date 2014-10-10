91a7978df90adf4bfee79ccf589fbbf5224d1707
Gilbert Sewovoe-Ekoue <gilberts55@hotmail.com>
2014-10-10 11:31:49 -0400

    updates

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,9 +4,9 @@ class Beer
 
 	def self.from_json(json)
 		Beer.new.tap do |b|
-			b.name        = json["name"]
-			b.description = json["description"]
-			b.image_url   = json["label"]["medium"] if json["label"]
+			b.name         = json["name"]
+			b.description  = json["description"]
+			b.image_url = json["label"]["medium"] if json["label"]
 		end
 	end
 end
--- server.rb
+++ server.rb
@@ -12,7 +12,29 @@ set :public_folder, File.join(File.dirname(__FILE__), 'public')
 
 get '/' do
 	@query = params[:q]
+	do_search @query if @query
 	erb :index
+end
+
+
+def do_search(q)
+	response = api.search(q)
+	if response.code == 200
+		# logger.info "Response: #{response.body}"
+		@beers = parse_beers(response)
+	else
+		logger.error = "ERROR: #{response.body}"
+		@error = response.body
+	end
+end
+
+def parse_beers(response)
+	return [] unless response["data"]
+
+	response["data"].map { |beer_json|
+		Beer.from_json(beer_json)
+		logger.info Beer
+	}
 end
 
 def api
